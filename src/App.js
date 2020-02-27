dc8c1ad1237e63fa3e679c9d86b27aa16cb0c1d5
eliseunogueira <eliseu.nogueira@outlook.com>
2020-02-27 10:33:45 -0300

    Chore: Scaffold the small project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {/**NewComment */}
+        <div>
+          <textarea cols='30' rows='10'></textarea>
+          <button>Send</button>
+        </div>
+        {/** Comments */}
+        <div>
+          {/**Comment */}
+          <div>Comment 1</div>
+          <div>Comment 2</div>
+          <div>Comment 3</div>
+          <div>Comment 4</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
