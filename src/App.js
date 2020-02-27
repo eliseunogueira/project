45e454de5f9bba1d1d909f063fdea67c35a6e5c2
eliseunogueira <eliseu.nogueira@outlook.com>
2020-02-27 10:47:14 -0300

    Chore: Scaffold send new Comment usind setState

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    NewComent: '',
+    comments: ['Comment 1', 'Comment 2', 'Comment 3', 'Comment 4']
+  };
+  sendComment = () => {
+    this.setState({
+      comments: [...this.state.comments, 'Comment']
+    });
+  };
   render() {
     return (
       <div>
         {/**NewComment */}
         <div>
           <textarea cols='30' rows='10'></textarea>
-          <button>Send</button>
+          <button onClick={this.sendComment}>Send</button>
         </div>
         {/** Comments */}
         <div>
           {/**Comment */}
-          <div>Comment 1</div>
-          <div>Comment 2</div>
-          <div>Comment 3</div>
-          <div>Comment 4</div>
+          {this.state.comments.map(c => {
+            return <div>{c}</div>;
+          })}
         </div>
       </div>
     );
